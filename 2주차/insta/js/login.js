332cdc2c22989d37c6c63508b48550d8abf7812c
Geuni <dlrmsgnl620@naver.com>
2022-03-11 15:05:15 +0900

    Add: 로그인기능 화면이동 구현

--- 2주차/insta/js/login.js
+++ 2주차/insta/js/login.js
@@ -3,16 +3,20 @@ const loginForm = document.querySelector(".login");
 
 loginForm.addEventListener("keyup", () => {
   const loginBtn = document.querySelector(".login__btn");
+  // const loginBtn = document.getElementById("getDisable");
   const loginEmail = document.querySelector(".login__email").value;
   const loginPassWord = document.querySelector(".login__pw").value;
 
+  console.dir(loginBtn.disabled);
   if (loginEmail.indexOf("@") >= 0 && loginPassWord.length >= 5) {
-    loginBtn.disable = false;
+    loginBtn.disabled = false;
     loginBtn.style.backgroundColor = "#0595f6";
     loginBtn.style.cursor = "pointer";
+    // loginBtn.removeAttr("disabled");
   } else if (!loginEmail || loginPassWord < 5) {
-    loginBtn.disable = true;
+    loginBtn.disabled = true;
     loginBtn.style.backgroundColor = "#c4e1fb";
     loginBtn.style.cursor = "default";
+    // loginBtn.attr("disabled");
   }
 });
